73539b8df5b75b317297e255562943a539b7eac0
zhangxiaoshuai <zhang_xiao_shuai@yeah.net>
2017-01-22 12:57:07 +0800

    aa

--- OExamResource/App_Start/RouteConfig.cs
+++ OExamResource/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace OExamResource
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
